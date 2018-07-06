c471b15aa8abe2cd41f78b9cccd175b7c345494b
Kapil-Gurnani <kapilgurnani4@gmail.com>
2018-07-06 11:29:54 +0530

    Add form for message

--- public/js/index.js
+++ public/js/index.js
@@ -14,4 +14,28 @@ var socket = io();
 
        socket.on('newMessage', function (message) {
             console.log('New message',message);
+            var li = jQuery('<li></li>');
+            li.text(`${message.from}: ${message.text}`);
+
+            jQuery('#messages').append(li);
+       });
+
+       socket.emit('createMessage', {
+           from: 'AK47',
+           text: 'HI'
+       }, function(data) {
+           console.log('Got it',data);
+       });
+
+       jQuery('#message-form').on('submit', function(e) {
+        e.preventDefault();
+
+        socket.emit('createMessage', {
+            from: 'USer',
+            text: jQuery('[name=message]').val()
+        }, function() {
+
+        });
+
+
        });
